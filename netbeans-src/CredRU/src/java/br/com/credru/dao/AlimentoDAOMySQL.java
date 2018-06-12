873e72007fb2f2cf0ec2722e1e51a74199ed7274
Fernando <bolodecenoura@alu.ufc.br>
2018-06-12 19:29:23 -0300

    DAO com SQL

--- netbeans-src/CredRU/src/java/br/com/credru/dao/AlimentoDAOMySQL.java
+++ netbeans-src/CredRU/src/java/br/com/credru/dao/AlimentoDAOMySQL.java
@@ -5,10 +5,33 @@
  */
 package br.com.credru.dao;
 
+import br.com.credru.model.Alimento;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Soriano
  */
-public class AlimentoDAOMySQL implements AlimentoDAO{
+public class AlimentoDAOMySQL extends Conexao implements AlimentoDAO{
     
+    public void Cadastrar(Alimento a){
+        super.open();
+        try{
+            String SQL = "INSERT INTO modulo (titulo, url, imagem) VALUES" + "(?,?,?)";
+            PreparedStatement ps = super.getConnection().prepareStatement(SQL);
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException("Falha ao cadastrar aliemnto", e);
+        }finally{
+            super.close();
+        }
+        
+    }
+
+
 }
