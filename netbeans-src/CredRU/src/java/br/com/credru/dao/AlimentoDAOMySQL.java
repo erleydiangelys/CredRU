cd529ba554c9a39d38b61e0f4ab7032a30fb5648
Fernando <bolodecenoura@alu.ufc.br>
2018-06-16 12:38:23 -0300

    merge

--- netbeans-src/CredRU/src/java/br/com/credru/dao/AlimentoDAOMySQL.java
+++ netbeans-src/CredRU/src/java/br/com/credru/dao/AlimentoDAOMySQL.java
@@ -7,7 +7,9 @@ package br.com.credru.dao;
 
 import br.com.credru.dao.AlimentoDAO;
 import br.com.credru.model.Alimento;
+import br.com.credru.model.Categoria;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -17,33 +19,57 @@ import java.util.List;
  * @author Soriano
  */
 public class AlimentoDAOMySQL extends Conexao implements AlimentoDAO{
-    
-    public void Cadastrar(Alimento a){
+      
+    @Override
+    public boolean setAlimento(Alimento a) {
         super.open();
+        boolean retorno=false;
         try{
-            String SQL = "INSERT INTO modulo (titulo, url, imagem) VALUES" + "(?,?,?)";
+            String SQL = "INSERT INTO alimento (nome, gramas, valorCalorico,carboidrato,proteina,lipidios,obervacao,categoria) VALUES" + "(?,?,?,?,?,?)";
             PreparedStatement ps = super.getConnection().prepareStatement(SQL);
+            //Setando as variaveis
+            ps.setString(1, a.getNome());
+            ps.setFloat(2, a.getGramas());
+            ps.setFloat(3, a.getValorCalorico());
+            ps.setFloat(4, a.getCarboidratos());
+            ps.setFloat(5, a.getProteinas());
+            ps.setFloat(6, a.getLipidios());
+            ps.setString(7, a.getObservacao());
+            ps.setInt(8, Categoria.getCodigo(a.getCategoria()));
             
+            //Executando
             ps.executeUpdate();
             ps.close();
-            
+            retorno=true;
         }catch(SQLException e){
             e.printStackTrace();
             throw new RuntimeException("Falha ao cadastrar aliemnto", e);
         }finally{
             super.close();
-        }
-        
-    }
-    
-    @Override
-    public boolean setAlimento(Alimento a) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return retorno;
+        } 
     }
 
     @Override
     public Alimento getAlimento(String nome) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Alimento a = new Alimento ();
+        
+        String SQL = "SELECT * FROM alimento WHERE nome like (?)";
+        try{
+            PreparedStatement ps = super.getConnection().prepareStatement(SQL);
+            ps.setString(1, '%' + nome + '%');
+            
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                a.setNome(rs.getNString("nome"));
+            }
+                       
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally{
+            super.close();
+        }
+        return a;
     }
 
     @Override
