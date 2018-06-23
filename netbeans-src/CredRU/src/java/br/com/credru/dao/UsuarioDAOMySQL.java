6bd7d2718e146c6320e72a81610c044646a3ae1f
nandoWill <bolodecenoura@alu.ufc.br>
2018-06-23 09:46:35 -0300

    DAO

--- netbeans-src/CredRU/src/java/br/com/credru/dao/UsuarioDAOMySQL.java
+++ netbeans-src/CredRU/src/java/br/com/credru/dao/UsuarioDAOMySQL.java
@@ -7,17 +7,43 @@ package br.com.credru.dao;
 
 import br.com.credru.dao.UsuarioDAO;
 import br.com.credru.model.Usuario;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  *
  * @author Soriano
  */
-public class UsuarioDAOMySQL implements UsuarioDAO{
+public class UsuarioDAOMySQL extends Conexao implements UsuarioDAO{
 
     @Override
     public Usuario getUsuario(String username, String senha) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Usuario u = new Usuario();
+        try{
+        String SQL = "SELECT * FROM alimento WHERE nome = (?) AND senha = (?)";        
+            PreparedStatement ps = super.getConnection().prepareStatement(SQL);
+            ps.setString(1,username);            
+            ps.setString(2,senha);
+            ps.close();
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                u.setNome(rs.getNString("nome"));
+                u.setUserName(rs.getNString("username"));
+                u.setCredito(rs.getInt("credito"));
+                u.setAtivo(rs.getBoolean("ativo"));
+                
+                
+            }
+                       
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally{
+            super.close();
+            
+        }
+        return u;
     }
 
     @Override
